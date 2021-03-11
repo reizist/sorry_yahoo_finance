f8ccd3c00d3fcb28b1333fd5c3d2f1d1d61dfffa
reizist <reizist@gmail.com>
2021-03-11 22:43:27 +0900

    Use URI.open instead of Kernel.open for remove warning: warning: calling URI.open via Kernel#open is deprecated, call URI.open directly or use URI#open with ruby 2.7

--- ruby/lib/sorry_yahoo_finance/http_client.rb
+++ ruby/lib/sorry_yahoo_finance/http_client.rb
@@ -4,12 +4,12 @@ require 'nokogiri'
 module HttpClient
   def self.get(url)
     begin
-      html = open(url).read
+      html = URI.open(url).read
     rescue URI::InvalidURIError
       url = URI.encode(url)
-      html = open(url, "Accept-Encoding" => "utf-8")
+      html = URI.open(url, "Accept-Encoding" => "utf-8")
     rescue Zlib::DataError
-      html = open(url, "Accept-Encoding" => "utf-8")
+      html = URI.open(url, "Accept-Encoding" => "utf-8")
     end
     Nokogiri::HTML(html, url)
   end
